09f784f45d20581f3a3ab44d60ff4b82d86da664
LiorAgami <33335288+LiorAgami@users.noreply.github.com>
2021-01-24 16:51:32 +0200

    Update spotify.js

--- spotify-clone-app/src/spotify.js
+++ spotify-clone-app/src/spotify.js
@@ -1,10 +1,7 @@
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/"; // development-uri
-const clientid = "5ec37b0a7b5749ab813e0171cd1717db"; // development-code
-
-// const redirectUri = "https://lior-dev.com/spotify-clone/"; // production-uri
-// const clientid = "a3636aafd5c54643bab2040f851225e0"; // production-code
+const clientid = ""; // paste your spotify client ID here
 
 const scopes = [
 	"user-read-currently-playing",
